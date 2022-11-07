d26fabc4474aa297547356e3b9ac54ce48f2e33d
dwl21 <nggus5@naver.com>
2022-11-07 17:20:37 +0900

    chore:spring boot 설정

---
+++ src/main/java/com/leo/jwpspringframe/JwpSpringFrameApplication.java
@@ -0,0 +1,13 @@
+package com.leo.jwpspringframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpSpringFrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpSpringFrameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leo/jwpspringframe/JwpSpringFrameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.jwpspringframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwpSpringFrameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
